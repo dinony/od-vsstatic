d7249bd98f82ea3b283ae6c164aa156ca2e43162
dinony <onur.dogangoenuel@gmail.com>
2017-04-25 12:21:42 +0200

    Move style in separate files

--- src/about/about.component.ts
+++ src/about/about.component.ts
@@ -2,65 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'od-about',
-  styles: [`
-    .container {
-      display: flex;
-      flex-direction: column;
-      height: 100vh;
-      justify-content: center;
-      align-items: center;
-    }
-
-    .panel {
-      display: flex;
-      flex-direction: column;
-      justify-content: space-between;
-      align-items: center;
-      height: 200px;
-      border: 1px solid #b3eaff;
-      padding: 40px;
-      border-radius: 2px;
-    }
-
-    .npm-cmd {
-      color: #b3eaff;
-    }
-
-    .github {
-      text-align: center;
-    }
-
-    .github-btn {
-      display: inline-block;
-      margin-top: 20px;
-      margin-bottom: 20px;
-      padding: 10px 20px;
-      background-color: white;
-      border-radius: 2px;
-      cursor: pointer;
-      color: #222b44;
-    }
-
-    a {
-      text-decoration: none;
-    }
-
-    .twitter-btn {
-      display: inline-block;
-      border: 1px dashed pink;
-      padding: 10px 20px;
-      border-radius: 20px;
-      cursor: pointer;
-      color: #b3eaff;
-      font-weight: 900;
-    }
-
-    .back-btn {
-      padding: 5px 10px;
-      color: #b3eaff;
-      font-size: 10px;
-    }
-  `],
+  styleUrls: ['src/about/about.component.css'],
   template: `
     <div class="container">
       <div class="panel">
